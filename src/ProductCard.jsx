6f12a72e4b21c181846bf7965831dfa8ef5bcdcb
eminbasbayan <emin.basbayan@icloud.com>
2024-11-20 10:19:10 +0300

    componentlerde javascript değişkenleri tanımlama

--- src/ProductCard.jsx
+++ src/ProductCard.jsx
@@ -5,17 +5,20 @@ const productCardCss = {
 };
 
 function ProductCard() {
+  const image =
+    'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg';
+
+  const title = 'Tisört';
+  const price = '100';
+  
   return (
     <div className="product-card" style={productCardCss}>
       <div className="product-image">
-        <img
-          src="https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg"
-          alt="product image"
-        />
+        <img src={image} alt="product image" />
       </div>
       <div className="product-info">
-        <b className="product-title">Tisört</b>
-        <span className="product-price">100₺</span>
+        <b className="product-title">{title}</b>
+        <span className="product-price">{price}₺</span>
       </div>
     </div>
   );
