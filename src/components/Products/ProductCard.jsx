97a77523024c9b6b1c1410ca00b0a8baaba3ba64
eminbasbayan <emin.basbayan@icloud.com>
2024-11-20 13:55:59 +0300

    dinamik button component

--- src/components/Products/ProductCard.jsx
+++ src/components/Products/ProductCard.jsx
@@ -10,7 +10,15 @@ function ProductCard(props) {
       <div className="product-bottom">
         <b className="product-title">{props.title}</b>
         <span className="product-price">{props.price}₺</span>
-        <Button />
+        <Button color="primary" size="xl" classNames="mt-2">
+          Sepete Ekle
+        </Button>
+        <Button color="danger" size="lg" classNames="mb-1">
+          Ürünü Sil
+        </Button>
+        <Button color="success" size="sm" classNames="ml-1">
+          Ürün Ekle
+        </Button>
       </div>
     </div>
   );
--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -1,5 +1,9 @@
-function Button() {
-  return <button>Sepete Ekle</button>;
+import './Button.css';
+
+function Button({ color, size, classNames, children }) {
+  const componentClassNames = `btn btn-${color} btn-${size} ${classNames}`;
+
+  return <button className={componentClassNames}>{children}</button>;
 }
 
 export default Button;
