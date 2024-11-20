cd0db4a62890ea371bf605b4479071eb34c10da9
eminbasbayan <emin.basbayan@icloud.com>
2024-11-20 15:43:30 +0300

    çoklu state ve çoklu function ile form yönetimi

--- src/components/Products/AddProduct.jsx
+++ src/components/Products/AddProduct.jsx
@@ -1,27 +1,76 @@
 import './AddProduct.css';
 import Button from '../UI/Button';
+import { useState } from 'react';
 
 function AddProduct() {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [price, setPrice] = useState('');
+  const [image, setImage] = useState('');
+
+  function handleTitleChange(event) {
+    setTitle(event.target.value);
+  }
+
+  function handleDescChange(event) {
+    setDescription(event.target.value);
+  }
+
+  function handlePriceChange(event) {
+    setPrice(event.target.value);
+  }
+
+  function handleImageChange(event) {
+    setImage(event.target.value);
+  }
+
   return (
-    <form className="add-product-form">
-      <label>
-        Title:
-        <input type="text" name="title" required />
-      </label>
-      <label>
-        Description:
-        <input type="text" name="title" required />
-      </label>
-      <label>
-        Price:
-        <input type="number" name="price" required />
-      </label>
-      <label>
-        Image URL:
-        <input type="text" name="imageUrl" required />
-      </label>
-      <Button color={'success'}>Yeni Ürün Ekle</Button>
-    </form>
+    <div className="add-new-product">
+      <h2 className="text-2xl font-bold mb-1">Yeni Ürün Ekle</h2>
+      <form className="add-product-form">
+        {title}
+        <label>
+          Title:
+          <input
+            type="text"
+            name="title"
+            onChange={handleTitleChange}
+            required
+          />
+        </label>
+        {description}
+        <label>
+          Description:
+          <input
+            type="text"
+            name="title"
+            onChange={handleDescChange}
+            required
+          />
+        </label>
+        {price}
+        <label>
+          Price:
+          <input
+            type="number"
+            name="price"
+            onChange={handlePriceChange}
+            required
+          />
+        </label>
+        {image}
+        <label>
+          Image URL:
+          <input
+            type="text"
+            name="imageUrl"
+            onChange={handleImageChange}
+            required
+          />
+        </label>
+        <Button color={'success'}>Yeni Ürün Ekle</Button>
+      </form>
+    </div>
   );
 }
 
