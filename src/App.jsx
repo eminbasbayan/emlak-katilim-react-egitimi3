271c69681238a86af7a0c269c3e0ed0ca53582b2
eminbasbayan <emin.basbayan@icloud.com>
2024-11-20 09:04:45 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,11 @@ import { useState } from 'react';
 
 function App() {
   const [state, setState] = useState('Emin Başbayan');
-  let fullName = 'Emin Başbayan';
-
-  function handleClick() {
-    fullName = 'Ahmet Demir';
-    console.log(fullName);
-    setState(fullName);
-  }
 
   return (
     <div className="wrapper">
       <p>{state}</p>
-      <button onClick={handleClick}>İsmi Güncelle</button>
+      <button onClick={() => setState('Ahmet Demir')}>İsmi Güncelle</button>
     </div>
   );
 }
