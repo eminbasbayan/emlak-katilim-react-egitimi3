8ecd2a690a9ccfa5ce5510a0b267398e3492d507
eminbasbayan <emin.basbayan@icloud.com>
2024-11-21 09:58:57 +0300

    updated

--- src/components/Products/ProductCard.jsx
+++ src/components/Products/ProductCard.jsx
@@ -1,14 +1,7 @@
-import { useState } from 'react';
 import Button from '../UI/Button';
 import './ProductCard.css';
 
 function ProductCard(props) {
- 
-
-  function handleClick() {
-    props.setTitle('New Title');
-  }
-
   return (
     <div className="product-card">
       <div className="product-image">
@@ -18,7 +11,7 @@ function ProductCard(props) {
         <b className="product-title">{props.title}</b>
         <span className="product-desc">{props.description}</span>
         <span className="product-price">{props.price}₺</span>
-        <Button onClick={handleClick}>Title Change</Button>
+        <Button>Sepete Ekle</Button>
       </div>
     </div>
   );
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -5,24 +5,13 @@ import AddProduct from './AddProduct';
 import './Products.css';
 
 function Products() {
-  const [title, setTitle] = useState("Title");
-
   return (
     <div className="products">
-      <h2 className='text-3xl font-bold mb-4'>Products</h2>
+      <h2 className="text-3xl font-bold mb-4">Products</h2>
       <AddProduct />
       <div className="products-wrapper">
         {productsData.map((product) => {
-          return (
-            <ProductCard
-              key={product.id}
-              image={product.image}
-              price={product.price}
-              description={product.description}
-              title={title}
-              setTitle={setTitle}
-            />
-          );
+          return <ProductCard key={product.id} {...product} />;
         })}
       </div>
     </div>
