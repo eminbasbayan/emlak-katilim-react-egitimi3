06160b9020612ec222bbc7d1fa5b93a4f675f57c
eminbasbayan <emin.basbayan@icloud.com>
2024-11-21 12:19:03 +0300

    fetch metot ile daha fetch işlemi

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -3,27 +3,34 @@ import { useState } from 'react';
 import AddProduct from './AddProduct';
 import ProductCard from './ProductCard';
 
-import { productsData } from '../../productsData';
-
 import './Products.css';
 import Modal from '../UI/Modal';
+import Button from '../UI/Button';
 
 function Products() {
-  const [products, setProducts] = useState(productsData);
+  const [products, setProducts] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   function addNewProduct(product) {
     setProducts([product, ...products]);
   }
 
+  function fetchProducts() {
+    fetch('https://fakestoreapi.com/products')
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }
+
   return (
     <div className="products">
       <h2 className="text-3xl font-bold mb-4">Products</h2>
       <AddProduct
-        productsData={productsData}
         addNewProduct={addNewProduct}
         setIsModalOpen={setIsModalOpen}
       />
+      <Button classNames={'mb-4'} onClick={fetchProducts}>
+        Fetch Products
+      </Button>
       <div className="products-wrapper">
         {products.map((product) => (
           <ProductCard key={product.id} {...product} />
